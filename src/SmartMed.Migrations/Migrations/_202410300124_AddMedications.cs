7e6ac164a148d71f40c6da1839dfdf692ddb1ff6
Laaya Yaghoubi <layayaghoubi@gmail.com>
2024-10-30 03:33:45 +0400

    feat(migration): add medication migration with fluent migrator.

---
+++ src/SmartMed.Migrations/Migrations/_202410300124_AddMedications.cs
@@ -0,0 +1,23 @@
+using FluentMigrator;
+
+namespace SmartMed.Migrations.Migrations;
+[Migration(202410300124)]
+public class _202410300124_AddMedications : Migration
+{
+    public override void Up()
+    {
+        Create.Table("Medications")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity().NotNullable()
+            .WithColumn("Name").AsString(50).NotNullable()
+            .WithColumn("Quantity").AsInt32().NotNullable()
+            .WithColumn("Type").AsInt16().NotNullable()
+            .WithColumn("CreationDate").AsDate().NotNullable()
+            .WithColumn("Code").AsString(30).NotNullable();
+
+    }
+
+    public override void Down()
+    {
+        Delete.Table("Medications");
+    }
+}
