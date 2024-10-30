2b47c21a49c7be2809c2c0f39a37f752e251fec3
Laaya Yaghoubi <layayaghoubi@gmail.com>
2024-10-30 17:19:35 +0400

    test(medication): add unit tests for GetAllAsync method

---
+++ test/SmartMed.Applications.Unit.Tests/Medications/Get/GetAllMedicationTests.cs
@@ -0,0 +1,55 @@
+using FluentAssertions;
+using SmartMed.Application.Medications.Contracts;
+using SmartMed.Domain.Entities.Medications;
+using SmartMed.Test.Tools.Infrastructure.DataBaseConfig;
+using SmartMed.Test.Tools.Infrastructure.DataBaseConfig.Unit;
+using SmartMed.Test.Tools.Medications;
+using SmartMed.Test.Tools.Medications.Create;
+
+namespace SmartMed.Applications.Unit.Tests.Medications.Get;
+
+public class GetAllMedicationTests : BusinessUnitTest
+{
+    private readonly IMedicationService _medicationService;
+
+    public GetAllMedicationTests()
+    {
+        _medicationService = MedicationServiceFactory.Create(SetupContext);
+    }
+    
+    [Fact]
+    public async Task GetAll_should_return_medication_count_properly()
+    {
+        var medication =
+            new MedicationBuilder().Build();
+        Save(medication);
+        var medication2 =
+            new MedicationBuilder().Build();
+        Save(medication2);
+        var medication3 =
+            new MedicationBuilder().Build();
+        Save(medication3);
+        
+        var actual = await _medicationService.GetAllAsync();
+
+      actual.Count.Should().Be(3);
+    }
+    
+
+    [Fact]
+    public async Task GetAll_should_return_medication_date_properly()
+    {
+        var medication =
+            new MedicationBuilder().Build();
+        Save(medication);
+
+        var actual = await _medicationService.GetAllAsync();
+
+        var actualMed = actual.Single();
+        actualMed.Name.Should().Be(medication.Name);
+        actualMed.Quantity.Should().Be(medication.Quantity);
+        actualMed.Type.Should().Be(medication.Type);
+        actualMed.Code.Should().Be(medication.Code);
+        actualMed.Id.Should().Be(medication.Id);
+    }
+}
